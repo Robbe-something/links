48ca215a8c43bb8f4680c41bf32ce8345a3aeb99
Robbe Leroy <leroyrobbe@icloud.com>
2025-06-21 19:28:56 +0200

    Refactor course page to fetch and display course details using Supabase

--- app/(authenticated)/course/[directory]/page.tsx
+++ app/(authenticated)/course/[directory]/page.tsx
@@ -1,8 +1,10 @@
 import {Metadata} from "next";
+import {createClient} from "@/utils/supabase/server";
+import {redirect} from "next/navigation";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
 
-export async function generateMetadata(
-    {params}: { params: Promise<{ directory: string }> }
-): Promise<Metadata> {
+export async function generateMetadata({params}: { params: Promise<{ directory: string }> }): Promise<Metadata> {
     const {directory} = await params
     return {
         title: `Links - ${decodeURIComponent(directory)}`
@@ -15,7 +17,31 @@ export default async function Page({
     params: Promise<{ directory: string }>
 }) {
     const {directory} = await params
-    return (
-        <p>{decodeURIComponent(directory)}</p>
-    )
+    const course = decodeURIComponent(directory)
+
+    const supabase = await createClient()
+
+    const {data, error} = await supabase.from('course')
+        .select(`
+            id,
+            name
+        `)
+        .eq('name', course)
+        .limit(1)
+        .maybeSingle()
+
+    if (error) {
+        redirect('/error')
+    }
+
+    if (!data) {
+        return (<>
+                <p>Course not found</p>
+                <Link href="/home"><Button>Go to your courses</Button></Link>
+            </>)
+    }
+
+    const course_id = data?.id
+
+    return (<p>{course_id}</p>)
 }
