e735b2661e2514580d6f08d3efa278f6e25bf8af
Robbe Leroy <leroyrobbe@icloud.com>
2025-09-02 22:36:34 +0200

    add fix

--- app/ui/coursesTable/CourseDialog.tsx
+++ app/ui/coursesTable/CourseDialog.tsx
@@ -142,7 +142,7 @@ export default function CourseDialog({
 
     // Add email to the list, fetch userId
     const handleAddEmail = async () => {
-        const email = emailInput.trim();
+        const email = emailInput.trim().toLowerCase();
         if (!email) return;
         try {
             // Validate email using zod
